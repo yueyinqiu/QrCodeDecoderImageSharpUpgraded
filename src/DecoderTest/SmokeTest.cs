2adee759577442f7c1f753c5e89128c48d361700
yueyinqiu <yueyinqiu@outlook.com>
2024-12-04 17:04:38 +0800

    fix

--- src/DecoderTest/SmokeTest.cs
+++ src/DecoderTest/SmokeTest.cs
@@ -1,14 +1,14 @@
 using NUnit.Framework;
+using QrCodeDecoderImageSharpUpgraded;
 
 namespace DecoderTest
 {
     public class SmokeTests
     {
-
         [Test]
         public void SmokeTest()
         {
-            QRCodeDecoderLibrary.QRDecoder decoder = new();
+            QRDecoder decoder = new();
             var result = decoder.ImageDecoder(SixLabors.ImageSharp.Image.Load("pass.png"));
 
             Assert.IsNotNull(result);
@@ -20,14 +20,13 @@ namespace DecoderTest
         [Test]
         public void DecoderTest()
         {
-            QRCodeDecoderLibrary.QRDecoder decoder = new ();
+            QRDecoder decoder = new();
             var result = decoder.ImageDecoder(SixLabors.ImageSharp.Image.Load("pass.png"));
 
             Assert.IsNotNull(result);
             Assert.IsNotNull(result.Length == 1);
-            var decodedResult = QRCodeDecoderLibrary.QRDecoder.ByteArrayToString(result[0]);
+            var decodedResult = QRDecoder.ByteArrayToString(result[0]);
             Assert.AreEqual(decodedResult, "Bugs Bunny\n07/27/1940");
         }
     }
-
 }
--- src/QRCodeDecoderLibrary/ReedSolomon.cs
+++ src/QRCodeDecoderLibrary/ReedSolomon.cs
@@ -1,5 +1,4 @@
 using System;
-using QRCodeSharedLibrary;
 
 namespace QrCodeDecoderImageSharpUpgraded
 {
